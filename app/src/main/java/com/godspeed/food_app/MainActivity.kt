aceea25582e0bb26bce2bbf9cf35d37c69d4e976
priyanshuborole <ppborole1762002@gmail.com>
2022-01-05 12:31:24 +0530

    added bottom navigation

--- app/src/main/java/com/godspeed/food_app/MainActivity.kt
+++ app/src/main/java/com/godspeed/food_app/MainActivity.kt
@@ -2,10 +2,24 @@ package com.godspeed.food_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.godspeed.food_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/godspeed/food_app/fragment/OrderHistoryFragment.kt
@@ -0,0 +1,13 @@
+package com.godspeed.food_app.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.godspeed.food_app.R
+
+
+class OrderHistoryFragment : Fragment(R.layout.fragment_order_history) {
+
+}
