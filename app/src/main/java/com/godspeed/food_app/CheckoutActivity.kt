c540de2720567ea9ac21155e5499a8fb89f04828
Aditya Bonde <aditya.bonde1152@gmail.com>
2022-01-07 19:41:50 +0530

    Ui Created

--- app/src/main/java/com/godspeed/food_app/CheckoutActivity.kt
+++ app/src/main/java/com/godspeed/food_app/CheckoutActivity.kt
@@ -2,10 +2,15 @@ package com.godspeed.food_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.godspeed.food_app.databinding.ActivityCheckoutBinding
 
 class CheckoutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCheckoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+       binding = ActivityCheckoutBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_checkout)
+        setContentView(binding.root)
+
+
     }
 }
---
+++ app/src/main/java/com/godspeed/food_app/Model/OrderedItem.kt
@@ -0,0 +1,6 @@
+package com.godspeed.food_app.Model
+
+data class OrderedItem(val Img:Int , val name:String, val price:Int ,val qty:Int){
+
+}
+
