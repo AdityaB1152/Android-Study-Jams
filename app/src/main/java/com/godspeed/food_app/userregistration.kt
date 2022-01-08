aa5f9ca194418c5439f70593e2adab9ddc3fb468
techtalks-crypto <impratikspatil@gmail.com>
2022-01-08 23:07:42 +0530

    registration UI
    registration UI

--- app/src/main/java/com/godspeed/food_app/userregistration.kt
+++ app/src/main/java/com/godspeed/food_app/userregistration.kt
@@ -5,15 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.godspeed.food_app.R
-import com.godspeed.food_app.data.OrderHistoryItem
-import com.godspeed.food_app.databinding.FragmentOrderHistoryBinding
-import com.godspeed.food_app.databinding.FragmentOtpBinding
 import com.godspeed.food_app.databinding.FragmentRegistrationBinding
 
-class registration : Fragment(R.layout.fragment_registration) {
+class userregistration : Fragment(R.layout.fragment_registration) {
     private lateinit var binding: FragmentRegistrationBinding
 
     override fun onCreateView(
